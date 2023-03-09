bbeac411c9d4fa76f3792709f0161570ee8810da
PRKDevelops <124041839+PRKDevelops@users.noreply.github.com>
2023-03-09 18:16:51 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,19 @@
+
+public class Rectangle {
+public static void Inverted_Half_Pyramid(int n){
+    for(int i = 1; i<=n; i++){
+    	//inner-numbers
+    	for(int j=1; j<= n-i+1; j++){
+    		System.out.print(j);
+    	}
+    	System.out.println();
+    	
+    	}
+    }
+    	
+
+	public static void main(String args[]){
+		Inverted_Half_Pyramid(5);
+		
+}
+}
